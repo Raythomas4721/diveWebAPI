20287e0db1b45cfcaf73749132ff7996ff70d5a8
cading7581 <yoclife.joan@gmail.com>
2025-02-14 16:55:12 +0800

    courseCRUD

--- Controllers/TCcoursesController.cs
+++ Controllers/TCcoursesController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using diveWebAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using diveWebAPI.DTO;
 
 namespace diveWebAPI.Controllers
 {
@@ -22,46 +24,83 @@ namespace diveWebAPI.Controllers
 
         // GET: api/TCcourses
         [HttpGet]
-        public async Task<IEnumerable<TCcourse>> GetTCcourses()
+        public async Task<IEnumerable<TCcourseDTO>> GetTCcourses()
         {
-            return await _context.TCcourses.ToListAsync();
+            return _context.TCcourses.Select(e=>new TCcourseDTO {
+                CourseId=e.CourseId,
+                CourseCategoryId=e.CourseCategoryId,
+                LevelId=e.LevelId,
+                CoachId=e.CoachId,
+                CoursePrice=e.CoursePrice,
+                Photo=e.Photo,
+                CreatedAt=e.CreatedAt,
+                UpdatedAt=e.UpdatedAt,
+                Discription=e.Discription,
+                CourseStatus=e.CourseStatus,
+                StartAt = e.StartAt
+            });
         }
 
         // GET: api/TCcourses/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<TCcourse>> GetTCcourse(int id)
+        public async Task<TCcourseDTO> GetTCcourse(int id)
         {
             var tCcourse = await _context.TCcourses.FindAsync(id);
 
             if (tCcourse == null)
             {
-                return NotFound();
+                return null;
             }
+            TCcourseDTO courseDTO = new TCcourseDTO
+            {
+                CourseId = tCcourse.CourseId,
+                CourseCategoryId = tCcourse.CourseCategoryId,
+                LevelId = tCcourse.LevelId,
+                CoachId = tCcourse.CoachId,
+                CoursePrice = tCcourse.CoursePrice,
+                Photo = tCcourse.Photo,
+                CreatedAt = tCcourse.CreatedAt,
+                UpdatedAt = tCcourse.UpdatedAt,
+                Discription = tCcourse.Discription,
+                CourseStatus = tCcourse.CourseStatus,
+                StartAt = tCcourse.StartAt
 
-            return tCcourse;
+            };
+            return courseDTO;
         }
 
         // PUT: api/TCcourses/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTCcourse(int id, TCcourse tCcourse)
+        public async Task<String> PutTCcourse(int id, TCcourseDTO courseDTO)
         {
-            if (id != tCcourse.CourseId)
+            if (id != courseDTO.CourseId)
             {
-                return BadRequest();
+                return "修改課程失敗";
             }
-
-            _context.Entry(tCcourse).State = EntityState.Modified;
+            TCcourse tCcourse = await _context.TCcourses.FindAsync(id);
+            tCcourse.CourseCategoryId=courseDTO.CourseCategoryId;
+            tCcourse.LevelId=courseDTO.LevelId;
+            tCcourse.CoachId=courseDTO.CoachId;
+            tCcourse.CoursePrice = courseDTO.CoursePrice;
+            tCcourse.Photo = courseDTO.Photo;
+            tCcourse.UpdatedAt = DateTime.Now; 
+            tCcourse.Discription= courseDTO.Discription;
+            tCcourse.CourseStatus= courseDTO.CourseStatus;
+            tCcourse.StartAt= courseDTO.StartAt;
+           
 
             try
             {
+                _context.TCcourses.Update(tCcourse);
                 await _context.SaveChangesAsync();
+                
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!TCcourseExists(id))
                 {
-                    return NotFound();
+                    return "修改課程資料庫失敗";
                 }
                 else
                 {
@@ -69,34 +108,55 @@ namespace diveWebAPI.Controllers
                 }
             }
 
-            return NoContent();
+            return "修改課程成功";
         }
 
         // POST: api/TCcourses
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<TCcourse>> PostTCcourse(TCcourse tCcourse)
+        public async Task<String> PostTCcourse(TCcourseDTO courseDTO)
         {
-            _context.TCcourses.Add(tCcourse);
-            await _context.SaveChangesAsync();
+            TCcourse tccourse = new TCcourse {
+                CourseId=0,
+                LevelId=courseDTO.LevelId,
+                CoachId=courseDTO.CoachId,
+                CoursePrice = courseDTO.CoursePrice,
+                Photo = courseDTO.Photo,
+                CourseCategoryId=courseDTO.CourseCategoryId,
+                UpdatedAt = DateTime.Now,
+                Discription= courseDTO.Discription,
+                CourseStatus= courseDTO.CourseStatus,
+                StartAt= courseDTO.StartAt
 
-            return CreatedAtAction("GetTCcourse", new { id = tCcourse.CourseId }, tCcourse);
+            };
+            _context.TCcourses.Add(tccourse);
+            await _context.SaveChangesAsync();
+            return $"課程編號:{tccourse.CourseId}";
+
+          
         }
 
         // DELETE: api/TCcourses/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTCcourse(int id)
+        public async Task<String> DeleteTCcourse(int id)
         {
             var tCcourse = await _context.TCcourses.FindAsync(id);
             if (tCcourse == null)
             {
-                return NotFound();
+                return "刪除課程失敗";
             }
+            try
+            {
+                _context.TCcourses.Remove(tCcourse);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex) 
+            {
+                return "刪除課程關聯記錄失敗";
+            }
+            
 
-            _context.TCcourses.Remove(tCcourse);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return "刪除成功";
         }
 
         private bool TCcourseExists(int id)
---
+++ DTO/TCcourseDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace diveWebAPI.DTO
+{
+    public class TCcourseDTO
+    {
+        public int CourseId { get; set; }
+
+        public int? CourseCategoryId { get; set; }
+
+        public int? LevelId { get; set; }
+
+        public int? CoachId { get; set; }
+
+        public decimal? CoursePrice { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string Discription { get; set; }
+
+        public bool? CourseStatus { get; set; }
+
+        public DateTime? StartAt { get; set; }
+    }
+}
