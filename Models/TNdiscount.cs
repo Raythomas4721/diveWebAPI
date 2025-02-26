d9a6ae3d0141be9f481cc6da5bfeb0db4e1efce4
cading7581 <yoclife.joan@gmail.com>
2025-02-26 12:28:03 +0800

    更新tndiscount

--- Models/TNdiscount.cs
+++ Models/TNdiscount.cs
@@ -20,5 +20,4 @@ public partial class TNdiscount
     public DateTime? EndDate { get; set; }
 
     public virtual TNproductCategory ProductCategory { get; set; }
-    public int? ProductCategoryId { get; set; } 
 }
